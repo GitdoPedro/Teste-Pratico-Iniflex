6124524097f543eeed45e8c53444bd1c79cfac70
GitdoPedro <emailpedrolima@gmail.com>
2023-05-03 21:06:37 -0300

    commit inicial

---
+++ src/test/java/br/com/projedata/Iniflex/IniflexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projedata.Iniflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
